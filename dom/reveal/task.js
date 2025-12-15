4eb5c4cf226ad0ce0e1195a1a6ebc64d07f21e21
Anastasia Konstantinova <konstantinova_anastasia@mail.ru>
2025-12-15 08:14:36 +0300

    second commit reveal

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,10 +1,13 @@
 function checkReveal() {
-    const element = document.querySelector('.reveal');
-    const position = element.getBoundingClientRect();
+    const elements = document.querySelectorAll('.reveal');
 
-    if (position.top < window.innerHeight && position.bottom > 0) {
-        element.classList.add('reveal_active');
-    }
+    elements.forEach(element => {
+        const position = element.getBoundingClientRect();
+
+        if (position.top < window.innerHeight && position.bottom > 0) {
+            element.classList.add('reveal_active');
+        }
+    });
 }
 
 window.addEventListener('scroll', checkReveal);
